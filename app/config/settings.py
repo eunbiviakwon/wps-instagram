10a46806d94738340481630ac24a3984d556e2ca
eunbiviakwon <eunbiviakwon@gmail.com>
2019-12-23 18:57:36 +0900

    Second commit

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ROOT_DIR = os.path.dirname(BASE_DIR)
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -123,4 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = ''
